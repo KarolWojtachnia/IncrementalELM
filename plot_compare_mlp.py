bf5277c3a1a38b43de6eea1f28020789b6c9e5d6
Joana <asiakom24@vp.pl>
2023-03-30 14:21:02 +0200

    plot

--- plot_compare_mlp.py
+++ plot_compare_mlp.py
@@ -17,23 +17,25 @@ ptimes = np.mean(ptimes, axis=0)
 cols = ['cornflowerblue', 'tomato', 'purple']
 k=1
 
-fig, ax = plt.subplots(3, 1, figsize=(10, 10))
+fig, ax = plt.subplots(3, 1, figsize=(10, 8), sharex=True)
 
 for m_id, m_label in enumerate(labels):
     ax[0].plot(gaussian_filter(scores[m_id], k), label=m_label, c=cols[m_id], alpha=0.5)
     ax[1].plot(scores_to_cummean(scores[m_id]), label=m_label, c=cols[m_id], alpha=0.5)
     ax[2].plot(scores_to_cummean(ptimes[m_id]), label=m_label, c=cols[m_id], alpha=0.5)
 
-ax[0].set_title('accuracy')
-ax[1].set_title('accumulated accuracy')
-ax[2].set_title('accumulated training time')
+ax[0].set_ylabel('accuracy')
+ax[2].set_xlabel('chunk')
+ax[1].set_ylabel('accumulated accuracy')
+ax[2].set_ylabel('accumulated training time')
 
 ax[0].set_ylim(0.5,1)
 ax[1].set_ylim(0.5,1)
+ax[2].set_xlim(0,249)
+ax[0].legend(frameon=False, ncol=3)
 
 for a in ax:
     a.grid(ls=':')
-    a.legend()
     a.spines['top'].set_visible(False)
     a.spines['right'].set_visible(False)
 
