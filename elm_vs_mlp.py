5e8f1dd1791895e2e68b8281f50ef7e58bda1979
Joana <asiakom24@vp.pl>
2023-03-30 13:32:40 +0200

    Compare - 5d

--- elm_vs_mlp.py
+++ elm_vs_mlp.py
@@ -1,135 +1,65 @@
 from time import time
-
-import matplotlib.pyplot as plt
 import strlearn as sl
-from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import accuracy_score
 from sklearn.neural_network import MLPClassifier
-
 from elm import ExtremeLearningMachine as elm
 from extremeMLP import ExtremeMLP
 import numpy as np
 
-if __name__ == "__main__":
-    stream = sl.streams.StreamGenerator(n_chunks=1000,
+random_states = np.random.randint(10,10000,10)
+n_chunks=250
+
+scores = np.zeros((len(random_states), 3, n_chunks)) #ELM, MLP, EELM
+ttimes = np.zeros((len(random_states), 3, n_chunks))
+ptimes = np.zeros((len(random_states), 3, n_chunks)) 
+
+for r_id, r in enumerate(random_states):
+    print(r_id)
+        
+    stream = sl.streams.StreamGenerator(n_chunks=n_chunks,
                                         chunk_size=1000,
                                         n_classes=2,
-                                        n_drifts=11,
-                                        n_features=10,
-                                        n_informative=10,
+                                        n_drifts=5,
+                                        n_features=20,
+                                        n_informative=20,
                                         n_redundant=0,
                                         n_repeated=0,
-                                        random_state=123456,
-                                        class_sep=0.8,
+                                        class_sep=0.5,
+                                        random_state = r,
                                         concept_sigmoid_spacing=999)
 
-    elm = elm(hidden_units=100, delta=.8)
-    mlp = MLPClassifier(hidden_layer_sizes=(100,))
-    ensemble = ExtremeMLP(hidden_units=100, delta=.8)
+    methods = [
+        elm(hidden_units=100, delta=.3),
+        MLPClassifier(hidden_layer_sizes=(100,)),
+        ExtremeMLP(hidden_units=100, delta=.3)]
 
-    scores_mlp = []
-    scores_elm = []
-    scores_ensemble = []
-
-    ttimes_mlp = []
-    ttimes_elm = []
-    ttimes_ensemble = []
-
-    ptimes_mlp = []
-    ptimes_elm = []
-    ptimes_ensemble = []
-
-    mlp_reps = 1
-
+    chunk_id = 0
     while chunk := stream.get_chunk():
         X, y = chunk
 
         # Test
         if stream.chunk_id > 0:
-            # ELM
-            a = time()
-            y_pred = elm.predict(X)
-            ptime_elm = time() - a
-            score_elm = balanced_accuracy_score(y, y_pred)
+            
+            for m_id, m in enumerate(methods):
+                a = time()
+                y_pred = m.predict(X)
+                ptime = time() - a
+                score = accuracy_score(y, y_pred)
 
-            # MLP
-            a = time()
-            y_pred = mlp.predict(X)
-            ptime_mlp = time() - a
-            score_mlp = balanced_accuracy_score(y, y_pred)
-
-            # ensemble
-            a = time()
-            y_pred = ensemble.predict(X)
-            ptime_ensemble = time() - a
-            score_ensemble = balanced_accuracy_score(y, y_pred)
-
-            scores_mlp.append(score_mlp)
-            scores_elm.append(score_elm)
-            scores_ensemble.append(score_ensemble)
-
-            print('[%04i] SCORE ELM-%.3f vs MLP-%.3f' % (
-                stream.chunk_id, score_elm, score_mlp))
-
-            ptimes_mlp.append(ptime_mlp)
-            ptimes_elm.append(ptime_elm)
-            ptimes_ensemble.append(ptime_ensemble)
+                scores[r_id, m_id, chunk_id]=score
+                ptimes[r_id, m_id,chunk_id]=ptime
 
         # Train
-        # MLP
-        a = time()
-        for j in range(mlp_reps):
-            mlp.partial_fit(X, y, classes=[0, 1])
-        ttime_mlp = time() - a
+        for m_id, m in enumerate(methods):
 
-        # ELM
-        a = time()
-        elm.partial_fit(X, y)
-        ttime_elm = time() - a
+            a = time()
+            m.partial_fit(X, y, classes=[0, 1])
+            ttime = time() - a
+            
+            ttimes[r_id, m_id,chunk_id]=ttime
+        
+        chunk_id+=1
 
-        # ensemble
-        a = time()
-        ensemble.partial_fit(X, y)
-        ttime_ensemble = time() - a
-
-        ttimes_mlp.append(ttime_mlp)
-        ttimes_elm.append(ttime_elm)
-        ttimes_ensemble.append(ttime_ensemble)
-
-    fig, ax = plt.subplots(3, 1, figsize=(14, 7))
-
-    ax[0].plot(scores_elm, label="ELMi")
-    ax[0].plot(scores_mlp, label="MLP")
-    ax[0].plot(scores_ensemble, label="ensemble")
-    ax[0].legend()
-    ax[0].grid(ls=":")
-    ax[0].set_ylim(0, 1)
-    ax[0].set_title('F1-score')
-
-    ax[1].plot(ttimes_elm, label="ELMi")
-    ax[1].plot(ttimes_mlp, label="MLP")
-    ax[1].plot(ttimes_ensemble, label="ensemble")
-    ax[1].legend()
-    ax[1].grid(ls=":")
-    ax[1].set_title('training times')
-
-    ax[2].plot(ptimes_elm, label="ELMi")
-    ax[2].plot(ptimes_mlp, label="MLP")
-    ax[2].plot(ptimes_ensemble, label="ensemble")
-    ax[2].legend()
-    ax[2].grid(ls=":")
-    ax[2].set_title('prediction times')
-
-    plt.savefig('Results/results_class_sep_08.png', format='png')
-    plt.close()
-    results = np.zeros((999, 3, 3))
-    results[:, 0, 0] = scores_mlp
-    results[:, 1, 0] = ttimes_mlp[1:]
-    results[:, 2, 0] = ptimes_mlp
-    results[:, 0, 1] = scores_elm
-    results[:, 1, 1] = ttimes_elm[1:]
-    results[:, 2, 1] = ptimes_elm
-    results[:, 0, 2] = scores_ensemble
-    results[:, 1, 2] = ttimes_ensemble[1:]
-    results[:, 2, 2] = ptimes_ensemble
-    np.save('Results/results_class_sep_08', results)
-
+np.save('res/scores_vs.npy', scores)
+np.save('res/ptimes_vs.npy', ptimes)
+np.save('res/ttimes_vs.npy', ttimes)
---
+++ plot_compare_mlp.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.ndimage import gaussian_filter
+
+def scores_to_cummean(scores):
+    return np.cumsum(scores)/np.arange(1,len(scores)+1)
+
+labels = ['ELM','MLP','Ensemble']
+scores = np.load('res/scores_vs.npy')
+ttimes = np.load('res/ttimes_vs.npy')
+ptimes = np.load('res/ptimes_vs.npy')
+
+scores = np.mean(scores, axis=0)
+ttimes = np.mean(ttimes, axis=0)
+ptimes = np.mean(ptimes, axis=0)
+
+cols = ['cornflowerblue', 'tomato', 'purple']
+k=1
+
+fig, ax = plt.subplots(3, 1, figsize=(10, 10))
+
+for m_id, m_label in enumerate(labels):
+    ax[0].plot(gaussian_filter(scores[m_id], k), label=m_label, c=cols[m_id], alpha=0.5)
+    ax[1].plot(scores_to_cummean(scores[m_id]), label=m_label, c=cols[m_id], alpha=0.5)
+    ax[2].plot(gaussian_filter(ptimes[m_id], k), label=m_label, c=cols[m_id], alpha=0.5)
+
+ax[0].set_title('accuracy')
+ax[1].set_title('Accumulated accuracy')
+ax[2].set_title('training times')
+
+ax[0].set_ylim(0.5,1)
+ax[1].set_ylim(0.5,1)
+
+for a in ax:
+    a.grid(ls=':')
+    a.legend()
+    a.spines['top'].set_visible(False)
+    a.spines['right'].set_visible(False)
+
+plt.tight_layout()
+plt.savefig('foo.png')
